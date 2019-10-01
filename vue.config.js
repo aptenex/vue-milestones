daeab86a77c227d74af8c9454eb95584e83a2dd9
Chris Atkinson <chrisacky@gmail.com>
2019-10-01 16:44:47 +0100

    Add dist

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)));
   },
-
+  filenameHashing: false,
   configureWebpack: {
     module: {
       rules: [
